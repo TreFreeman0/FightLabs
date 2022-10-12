561ec0e862185ab71d97327d3574e1d00ab20316
Trevon Freeman <trevonfreeman16@gmail.com>
2022-10-12 07:11:12 -0700

    Fixed javascript issue and some styling issue

--- index.js
+++ index.js
@@ -12,26 +12,27 @@ const hiddenElementsY= document.querySelectorAll(".hideY")
 const hiddenElementsX = document.querySelectorAll(".hideR")
 hiddenElements.forEach((el) => observer.observe(el));
 hiddenElementsY.forEach((el) => observer.observe(el));
-hiddenElementsX.forEach((el) => observer.observe(el))
+hiddenElementsX.forEach((el) => observer.observe(el));
 
 
 // Form Validation
+// Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
